17f05a323c0f03b185582f2b01aac9a9b188c4f8
Nimit <nimitagg95@gmail.com>
2019-12-12 21:02:42 +0530

    Updates

---
+++ precache-manifest.6892119ce582366dd8e31c59e21163d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386253455f4cbab7fd1e2767cf3e65a5",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "2c8845855bd4e2abbc12",
+    "url": "/React-todo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e189cf6eb985fa94edf0",
+    "url": "/React-todo/static/js/2.c3d17641.chunk.js"
+  },
+  {
+    "revision": "2c8845855bd4e2abbc12",
+    "url": "/React-todo/static/js/main.d6704977.chunk.js"
+  },
+  {
+    "revision": "d27cdd870224b76c07c9",
+    "url": "/React-todo/static/js/runtime-main.cddaba4f.js"
+  }
+]);
